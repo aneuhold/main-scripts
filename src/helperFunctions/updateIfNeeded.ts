073e6a2e73324e7e5ebf9b2bfcebade5ac4fe64e
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-10-01 09:31:41 -0700

    Add prettier config

--- src/helperFunctions/updateIfNeeded.ts
+++ src/helperFunctions/updateIfNeeded.ts
@@ -1,7 +1,7 @@
 import { exec } from "child_process";
 import { name as PACKAGE_NAME } from "../../package.json";
-import Store, { KEYS } from '../Store';
-import { datesAreOnSameDay } from './dateFunctions';
+import Store, { KEYS } from "../Store";
+import { datesAreOnSameDay } from "./dateFunctions";
 
 /**
  * Triggers an update of this package.
@@ -30,9 +30,10 @@ async function hasAlreadyBeenUpdatedToday(): Promise<boolean> {
   if (!lastCheckDate) {
     await Store.set(KEYS.lastUpdateCheckDate, new Date());
     return false;
-  } if (datesAreOnSameDay(lastCheckDate, new Date())) {
+  }
+  if (datesAreOnSameDay(lastCheckDate, new Date())) {
     return true;
-  } 
+  }
   return false;
 }
 
