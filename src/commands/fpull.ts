3529f7551bc71327e732069fe985238c96c66a17
Anton G. Neuhold Jr <agneuhold@gmail.com>
2021-10-05 06:02:50 -0700

    Update order of logging

--- src/commands/fpull.ts
+++ src/commands/fpull.ts
@@ -3,8 +3,8 @@ import Log from '../helperFunctions/logger';
 
 export default async function fpull(): Promise<void> {
   const { didComplete, output: output1 } = await execCmd(`git fetch -a`);
-  Log.info(output1);
   if (didComplete) {
+    Log.info(output1);
     const { output: output2 } = await execCmd(`git pull`);
     Log.info(output2);
   }
