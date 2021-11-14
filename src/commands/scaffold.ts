1c28e41d938cd811d5aad3269e9898a2e814055f
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-11-14 00:31:37 -0800

    Add working user input

--- src/commands/scaffold.ts
+++ src/commands/scaffold.ts
@@ -1,5 +1,7 @@
 import Log from '../helperFunctions/Log';
+import getUserInput from '../helperFunctions/input';
 
 export default async function scaffold(args: string[]): Promise<void> {
   Log.info(`Args provided were: ${args}`);
+  console.log(await getUserInput('What is the name of the project?'));
 }
