8f3cfa5a16a5c9e189a823c10700e871570f33e4
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-06-23 07:08:15 -0700

    Finish up aliase command update

--- src/config/aliases.ts
+++ src/config/aliases.ts
@@ -4,10 +4,13 @@ export type AliasInfo = {
   [osType in OperatingSystemType]: string;
 };
 
+/**
+ * The list of different aliases that should be set on all operating systems.
+ */
 const aliases: { [command: string]: AliasInfo } = {
   ctb: {
     [OperatingSystemType.Windows]:
-      `Function ctbFunc {Set-Location -Path "C:\\Users\\Anton G Neuhold Jr\\Desktop\\Development"}\n` +
+      `Function ctbFunc {ssh aneuhold@137.184.231.32}\n` +
       `Set-Alias -Name ctb -Value ctbFunc`,
     [OperatingSystemType.MacOSX]: '',
     [OperatingSystemType.Linux]: '',
